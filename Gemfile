c49ad4a4aba3229c4ecb366530bf736744033aa7
Hiroshi Ota <otahi.pub@gmail.com>
2015-12-06 06:52:09 +0900

    Add Serverspec simple test

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'trema', git: 'https://github.com/trema/trema.git', branch: 'develop'
+gem 'serverspec', '~> 2.24', '>= 2.24.3'
---
+++ spec/192.168.8.6/network_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe interface('nshost1') do
+  it { should exist }
+end
+
+describe host('192.168.8.7') do
+  # ping
+  it { should be_reachable }
+end
